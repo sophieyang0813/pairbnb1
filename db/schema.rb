e561fe599223924fd588a1227849b4aeb0391c4b
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-10-22 15:45:26 +0800

    installed Figaro Gem for privacy protection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020054034) do
+ActiveRecord::Schema.define(version: 20171020074710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171020054034) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
