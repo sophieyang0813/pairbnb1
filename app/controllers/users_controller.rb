ea3952fbf746d82c682a93074a7e5ae040727244
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-10-23 18:52:01 +0800

    statics controller and home.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < Clearance::UsersController
 
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      # render template: "users/new"
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+
+
+
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    root "users#new"
+    root "statics#home"
   resources :passwords, controller: "passwords", only: [:create, :new, :edit]
   resource :session, controller: "sessions", only: [:create]
 
