a737976aa85713d0ea7b79eb4412a925247fc803
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-10-24 12:15:57 +0800

    created new and host erb pages and wrote listing controller new and create method in controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,25 @@
 class ListingsController <  ApplicationController
 
     def new
-
+        @listing = Listing.new
     end
 
+    def create
+
+        @listing =Listing.new(listing_params)
+        # byebug
+        @listing.user_id = current_user.id
+
+        if @listing.save
+            render 'host'
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+        def listing_params
+            params.require(:listing).permit(:guest_num, :location, :room_type)
+        end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ApplicationRecord
     belongs_to :user
 
+
+
 end
