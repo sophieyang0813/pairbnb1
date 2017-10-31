02e0710db31d15eb2d20e2e695686d5ee09a42f3
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-10-31 14:47:29 +0800

    wrote 1) booking email method in reservation_mailer and 2) booking_email.html.erb

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,7 +1,18 @@
 class ReservationMailer < ApplicationMailer
+	default from: 'notifications@pairbnb.com'
 
 
 	def booking_email(customer, host, reservation_id)
+		@customer = customer 
+		@host =  host 
+		@reservation_id = reservation_id
+		@url = 'http://localhost:3000/sign_in'
+		@reservation = Reservation.find(@reservation_id)
+
+
+
+		mail(to: @host.email,
+         subject: 'New Booking from pairbnb')
 	end 
 
 end
