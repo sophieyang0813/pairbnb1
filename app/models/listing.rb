4f287db32757365f50545254c89413b55d1028d9
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-11-01 10:42:57 +0800

    add search engine form

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,10 @@ class Listing < ApplicationRecord
     has_many :reservations
     #185 reservation many-to-many? 1) created the model reservation, 2) establish many to many among listing, user, and reservation
 
+    #195A see index method 
+  scope :price_range, -> (price) { where price_per_night: price }
+  scope :city, -> (location) { where location: location }
+  scope :starts_with, -> (name) { where("name like ?", "#{name}%")}
+
+
 end
